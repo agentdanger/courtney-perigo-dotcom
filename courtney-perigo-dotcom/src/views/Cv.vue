4d2829207e17a92954a15c90246ced87f9974821
Courtney Perigo <11284316+agentdanger@users.noreply.github.com>
2023-10-08 19:14:36 -0500

    built cv page.

--- courtney-perigo-dotcom/src/views/Cv.vue
+++ courtney-perigo-dotcom/src/views/Cv.vue
@@ -2,29 +2,300 @@
     <AppNavBar />
 
     <section class="hero is-fullheight is-primary">
-        
-        <div class="hero-body">
-            <section class="section">
-                <div class="container">
-                    <h1 class="title">Section</h1>
-                <h2 class="subtitle">
-                    A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading.
-                </h2>
 
-                </div>
-                
-            </section>
+        <div class="hero-body">
             <div class="container">
-                <h1 class="title">
-                    James "Courtney" Perigo
-                </h1>
-                <h2 class="subtitle has-text-info">
-                    Expert Data Science Leader Driving Innovation for Business Success
-                </h2>
+                <div class="box p-1 mt-6">
+                    <div class="card has-background-primary">
+                        <header class="card-header ">
+                            <p class="card-header-title has-text-white is-size-4">
+                                Profile
+                            </p>
+                        </header>
+                        <div class="card-content">
+                            <div class="content has-text-white">
+                                Data science leader and practitioner with decades of experience turning data into
+                                insights that drive ROI.
+                                An innovator, building proprietary solutions that use machine learning and data engineering
+                                to generate millions in value for business.
+                                My goal is to affect operations, sales, and marketing through innovative, powerful, and
+                                scaled decision analytics product.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="box p-1 mt-1">
+                    <div class="card has-background-primary">
+                        <header class="card-header ">
+                            <p class="card-header-title has-text-white is-size-4">
+                                Resources
+                            </p>
+                        </header>
+                        <div class="card-content">
+                            <div class="buttons is-centered ">
+                                <a href="https://github.com/agentdanger" target="_blank" rel="noopener noreferrer" class="button is-link">
+                                    <span class="icon">
+                                        <i class="fab fa-github"></i>
+                                    </span>
+                                    <span>Github</span>
+                                </a>
+                                <a href="https://courtney-perigo.medium.com/" target="_blank" rel="noopener noreferrer" class="button is-link">
+                                    <span class="icon">
+                                        <i class="fab fa-medium"></i>
+                                    </span>
+                                    <span>Medium</span>
+                                </a>
+                                <a href="../assets/Perigo_James_Courtney_resume.pdf" download class="button is-link">
+                                    <span class="icon">
+                                        <i class="fas fa-file-pdf"></i>
+                                    </span>
+                                    <span>Download Resume</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="box p-1 mt-1 has-text-centered">
+                    <div class="card has-background-primary has-text-centered">
+                        <header class="card-header ">
+                            <p class="card-header-title has-text-white is-size-4">
+                                Skills
+                            </p>
+                        </header>
+                        <div class="card-content ">
+                            <div class="content">
+                                <div class="buttons is-centered ">
+                                    <button class="button is-primary">
+                                        <span class="icon">
+                                            <i class="fas fa-flask"></i>
+                                        </span>
+                                        <span>Applied Data Science</span>
+                                    </button>
+                                    <button class="button is-primary">
+                                        <span class="icon">
+                                            <i class="fas fa-wrench"></i>
+                                        </span>
+                                        <span>Data Engineering</span>
+                                    </button>
+                                    <button class="button is-primary">
+                                        <span class="icon">
+                                            <i class="fas fa-robot"></i>
+                                        </span>
+                                        <span>AI and ML Deployment</span>
+                                    </button>
+                                    <button class="button is-primary">
+                                        <span class="icon">
+                                            <i class="fas fa-chart-line"></i>
+                                        </span>
+                                        <span>Business Intelligence Architecture</span>
+                                    </button>
+                                    <button class="button is-primary">
+                                        <span class="icon">
+                                            <i class="fas fa-cloud"></i>
+                                        </span>
+                                        <span>Cloud Infrastructure</span>
+                                    </button>
+                                    <button class="button is-primary">
+                                        <span class="icon">
+                                            <i class="fab fa-python"></i>
+                                        </span>
+                                        <span>Python</span>
+                                    </button>
+                                    <button class="button is-primary">
+                                        <span class="icon">
+                                            <i class="fas fa-chart-bar"></i>
+                                        </span>
+                                        <span>R</span>
+                                    </button>
+                                    <button class="button is-primary">
+                                        <span class="icon">
+                                            <i class="fas fa-database"></i>
+                                        </span>
+                                        <span>SQL</span>
+                                    </button>
+                                    <button class="button is-primary">
+                                        <span class="icon">
+                                            <i class="fas fa-laptop-code"></i>
+                                        </span>
+                                        <span>GoLang</span>
+                                    </button>
+                                    <button class="button is-primary">
+                                        <span class="icon">
+                                            <i class="fab fa-js"></i>
+                                        </span>
+                                        <span>Javascript</span>
+                                    </button>
+                                    <button class="button is-primary">
+                                        <span class="icon">
+                                            <i class="fas fa-battery-full"></i>
+                                        </span>
+                                        <span>Servant Leadership</span>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="box p-1 mt-1">
+                    <div class="card has-background-primary">
+                        <header class="card-header ">
+                            <p class="card-header-title has-text-white is-size-4">
+                                Education
+                            </p>
+                        </header>
+                        <div class="card-content">
+                            <div class="content has-text-white">
+                                <div class="columns">
+                                    <div class="column">
+                                        <article class="media">
+                                            <figure class="media-left">
+                                                <figure class="image is-96x96">
+                                                    <img src="../assets/Northwestern_University_seal.png"
+                                                    alt="Northwestern University Seal">
+                                                </figure>
+                                            </figure>
+                                            <div class="media-content">
+                                                <div class="content">
+                                                    <p class="title is-4 has-text-white">Master of Science, Data Science</p>
+                                                    <p class="subtitle is-6 has-text-white">Northwestern University</p>
+                                                    <p class="subtitle is-6 has-text-white">2020 - 2022</p>
+                                                </div>
+                                            </div>
+                                        </article>
+                                    </div>
+                                    <div class="column">
+                                        <article class="media">
+                                            <figure class="media-left">
+                                                <figure class="image is-96x96">
+                                                    <img src="../assets/University_of_South_Florida_seal.svg"
+                                                    alt="University of South Florida Seal">
+                                                </figure>
+                                            </figure>
+                                            <div class="media-content">
+                                                <div class="content">
+                                                    <p class="title is-4 has-text-white">Master of Business Administration</p>
+                                                    <p class="subtitle is-6 has-text-white">University of South Florida</p>
+                                                    <p class="subtitle is-6 has-text-white">2007 - 2009</p>
+                                                </div>
+                                            </div>
+                                        </article>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="box p-1 mt-1">
+                    <div class="card has-background-primary">
+                        <header class="card-header ">
+                            <p class="card-header-title has-text-white is-size-4">
+                                Experience
+                            </p>
+                        </header>
+                        <header class="card-header">
+                            <p class="card-header-title has-text-white">
+                                <span class="has-text-info mr-1"> SVP, Executive Director of Marketing and Data
+                                    Sciences</span> | Cramer-Krasselt | 2021 - Present
+                            </p>
+                        </header>
+                        <div class="card-content">
+                            <div class="content has-text-white">
+                                Lead the agency's global data science and analytics capabilities with an integrated team
+                                across three offices. Build data products, architect analytics microservices, and develop
+                                data-driven models for marketing optimization and personalization - creating $5MM+ in annual
+                                recurring revenue for the agency. Won the Drum Digital Advertising Award for our novel
+                                curve-fit optimization model for marketing attribution.
+                            </div>
+                        </div>
+                        <header class="card-header ">
+                            <p class="card-header-title has-text-white">
+                                <span class="has-text-info mr-1"> SVP, Director of Data Strategy and Utilization</span> |
+                                Cramer-Krasselt | 2018 - 2021
+                            </p>
+                        </header>
+                        <div class="card-content">
+                            <div class="content has-text-white">
+                                Re-aligned an analytics team, and the tech stack, for an agile analytics approach
+                                progressing the agency through a digital transformation for shared intelligence across
+                                finance, operations and delivery. Built a predictive benchmarking capability that automates
+                                analysis for C-K's performance marketing teams - generating a consistent 5-10% increase in
+                                paid marketing performance.
+                            </div>
+                        </div>
+                        <header class="card-header ">
+                            <p class="card-header-title has-text-white">
+                                <span class="has-text-info mr-1"> VP, Director of Decision Sciences</span> | Publicis, Moxie
+                                | 2014 - 2018
+                            </p>
+                        </header>
+                        <div class="card-content">
+                            <div class="content has-text-white">
+                                Recruited and developed top analytics and data science talent to serve the agency's largest
+                                accounts including eBay, Kohl's, and Verizon Wireless. Engineered a real-time Hidden Markov
+                                Model that estimates the position of a prospect in the sales funnel for Verizon Wireless.
+                                Won a Golden Echo Award for Best Use of Data.
+                            </div>
+                        </div>
+                        <header class="card-header ">
+                            <p class="card-header-title has-text-white">
+                                <span class="has-text-info mr-1"> Director, Analytics</span> | Triad Retail Media, Walmart |
+                                2012 - 2014
+                            </p>
+                        </header>
+                        <div class="card-content">
+                            <div class="content has-text-white">
+                                Streamlined reporting and business intelligence, reducing report turnaround times from three
+                                days to on-demand by implementing Tableau and a PostgreSQL Data Warehouse in support of the
+                                sales department. Helped @WalmartLabs in the development of attribution solutions that link
+                                credit card transactions to omni-channel shopper marketing efforts - helping companies like
+                                P&G and Pepsi understand the ROI of their retail marketing efforts.
+                            </div>
+                        </div>
+                        <header class="card-header ">
+                            <p class="card-header-title has-text-white">
+                                <span class="has-text-info mr-1"> Market Research Manager, Mobile Healthcare
+                                    Solutions</span> | Sage Software | 2010 - 2012
+                            </p>
+                        </header>
+                        <div class="card-content">
+                            <div class="content has-text-white">
+                                Developed the product roadmap for the company's first mobile electronic health records
+                                solution for ambulatory care practices. Curated the backlog of product features for software
+                                developers, helping the team focus on key features suggested from conjoint-analysis and
+                                pricing exercises in primary market research and survey analyses.
+                            </div>
+                        </div>
+                        <header class="card-header ">
+                            <p class="card-header-title has-text-white">
+                                <span class="has-text-info mr-1"> Manager, Analytics</span> | Triad Retail Media, Walmart |
+                                2009 - 2010
+                            </p>
+                        </header>
+                        <div class="card-content">
+                            <div class="content has-text-white">
+                                Built the processes for business intelligence for the start-up, helping the sales team
+                                achieve
+                                $200 million in annual billings.
+                            </div>
+                        </div>
+                        <header class="card-header ">
+                            <p class="card-header-title has-text-white">
+                                <span class="has-text-info mr-1"> Sr. Market Analyst</span> | Media General, The Tampa
+                            Tribune | 2005 - 2009
+                        </p>
+                    </header>
+                    <div class="card-content">
+                        <div class="content has-text-white">
+                            Conducted statistical analysis, and developed data visualizations in support of news and
+                            sales
+                            teams with emphasis on geographic information systems and mapping.
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </section>
-</template>
+    </div>
+</section></template>
 
 <script>
 import AppNavBar from '../components/AppNavBar.vue'
