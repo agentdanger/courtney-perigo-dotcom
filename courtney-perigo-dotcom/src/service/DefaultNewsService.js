2de2424fcb72e6f212c9d0dfb1cda5b2b0d87fb7
Courtney Perigo <11284316+agentdanger@users.noreply.github.com>
2023-11-19 16:20:10 -0600

    adding new search capability on news page.

--- courtney-perigo-dotcom/src/service/DefaultNewsService.js
+++ courtney-perigo-dotcom/src/service/DefaultNewsService.js
@@ -7,6 +7,45 @@ export default class defaultNewsService {
         year: "numeric",
       });
     }
+
+    async getCustomNews(query) {
+      async function fetchWithTimeout(resource, options = {}) {
+        const { timeout = 30000 } = options;
+  
+        const controller = new AbortController();
+        const id = setTimeout(() => controller.abort(), timeout);
+        const response = await fetch(resource, {
+          ...options,
+          signal: controller.signal
+        });
+        clearTimeout(id);
+        return response;
+      }
+      try {
+        const response = await fetchWithTimeout(`https://getmachinanovanews-dot-personal-website-35.uc.r.appspot.com/get-custom-news/v1?query=${query}`)
+        const d = await response.json()
+        // remove items with a date not equal to today in constant format
+        const today = this.formatDate(new Date())
+        // turn string entry_published "Sat, 18 Nov 2023 09:46:04 GMT" into constant format
+        // turn string entry_published "Thu, 19 Oct 2023 07:00:50 +0100" into constant format
+        const filtered = d.filter(item => this.formatDate(new Date(item.entry_published)) === today)
+        // limit article summary to 100 words 
+        filtered.forEach(item => {
+          item.entry_summary = item.entry_summary.split(' ').slice(0, 75).join(' ')
+        })
+        // remove certain topics when they are in the title lowercase
+        const topicsToRemove = ['homework help', 'sponsored content', 'paid content']
+        const filtered2 = filtered.filter(item => !topicsToRemove.some(topic => item.entry_title.toLowerCase().includes(topic.toLowerCase())))
+        // remove certain topics when they are in the summary lowercase
+        const topicsToRemove2 = ['homework help', 'sponsored content', 'paid content']
+        const filtered3 = filtered2.filter(item => !topicsToRemove2.some(topic => item.entry_summary.toLowerCase().includes(topic.toLowerCase())))
+        return filtered3;
+      } catch (error) {
+        console.log(error)
+        throw new Error('Error in getDefaultNews fetch.')
+      }
+  
+    }
   
     async getDefaultNews() {
       async function fetchWithTimeout(resource, options = {}) {
--- courtney-perigo-dotcom/src/views/News.vue
+++ courtney-perigo-dotcom/src/views/News.vue
@@ -1,14 +1,5 @@
 <template>
-    <div v-if="newsServiceLoading">
-        <div class="modal-background has-background-white"></div>
-        <div class="modal-content">
-            <p class="image is-128x128">
-                <img src="/assets/brittany.jpg" alt="">
-            </p>
-        </div>
-    </div>
     <AppNavBar />
-
     <div class="hero is-fullheight is-primary">
         <div class="hero-body">
             <div class="container">
@@ -23,11 +14,34 @@
                             <div class="content has-text-white">
                                 {{ about_machinanova }}
                             </div>
+                            <div class="content has-text-white">
+                                <div class="field has-addons">
+                                    <div class="control is-expanded">
+                                        <input class="input" v-model="queryValue" type="text" placeholder="Enter a topic to find news">
+                                    </div>
+                                    <div class="control">
+                                        <button class="button is-link" @click="findNews(queryValue)">
+                                            <span class="icon">
+                                                <i class="fas fa-search"></i>
+                                            </span>
+                                            <span>Find News</span>
+                                        </button>
+                                    </div>
+                                    <div class="control">
+                                        <button class="button is-link is-light" @click="removeCustomNews">
+                                            <span class="icon">
+                                                <i class="fas fa-times"></i>
+                                            </span>
+                                            <span>Cancel</span>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <!-- show first 25 articles retrieved -->
-                <span v-for="(article, index) in articlesToDisplay" :key="index">
+                <span v-for="(article, index) in articlesToDisplay" :key="index" v-if="!customNewsActive">
                     <div class="box p-1 mt-6">
                         <div class="card has-background-white">
                             <header class="card-header has-background-primary">
@@ -44,8 +58,55 @@
                                     </div>
                                     <div class="media-content ">
                                         <p class="title is-size-5 has-text-grey-dark">{{ article.feed_title }}</p>
-                                        <p class="subtitle is-size-6 has-text-grey-dark">{{ article.entry_summary }}</p>                                        
+                                        <p class="subtitle is-size-6 has-text-grey-dark">{{ article.entry_summary }}</p>
+                                        <div class="tags">
+                                            <!-- show only first 4 tags -->
+                                            <span v-for="(tag, index) in article.entry_tags.slice(0, 4)" :key="index"
+                                                class="tag is-info">
+                                                {{ tag.term }}
+                                            </span>  
+                                        </div>                                   
                                     </div>
+                                    
+                                </div>
+                                
+                            </div>
+                            <div class="card-footer">
+                                    <a :href="article.entry_link" target="_blank" rel="noopener noreferrer"
+                                        class="card-footer-item has-background-link has-text-white">
+                                        <span>Read More</span>
+                                    </a>
+                            </div>
+                        </div>
+                    </div>
+                </span>
+                <span v-for="(article, index) in customArticlesToDisplay" :key="index" v-if="customNewsActive">
+                    <div class="box p-1 mt-6">
+                        <div class="card has-background-white">
+                            <header class="card-header has-background-primary">
+                                <p class="card-header-title is-size-4 has-text-white">
+                                    {{ article.entry_title }}
+                                </p>
+                            </header>
+                            <div class="card-content">
+                                <div class="media">
+                                    <div class="media-left">
+                                        <figure class="image is-128x128">
+                                            <img :src="article.entry_image.url" alt="Placeholder image">
+                                        </figure>
+                                    </div>
+                                    <div class="media-content ">
+                                        <p class="title is-size-5 has-text-grey-dark">{{ article.feed_title }}</p>
+                                        <p class="subtitle is-size-6 has-text-grey-dark">{{ article.entry_summary }}</p>
+                                        <div class="tags">
+                                            <!-- show only first 4 tags -->
+                                            <span v-for="(tag, index) in article.entry_tags.slice(0, 4)" :key="index"
+                                                class="tag is-info">
+                                                {{ tag.term }}
+                                            </span>  
+                                        </div>                                   
+                                    </div>
+                                    
                                 </div>
                                 
                             </div>
@@ -98,8 +159,11 @@ export default {
     },
     data() {
         return {
+            queryValue: null,
+            customNewsActive: false,
             defaultNewsServiceGet: null,
             articlesToDisplay: null,
+            customArticlesToDisplay: null,
             newsServiceLoading: false,
             about_machinanova: "This is a simple application that fetches recommended, recent data science news. \
             You can also type in your own data science topic and the model will pull recommended news in \
@@ -113,6 +177,25 @@ export default {
                 link: 'https://github.com/agentdanger/news-retriever'
             }
         }
+    },
+    methods: {
+        findNews(query) {
+            console.log(query)
+            this.customNewsActive = true
+            this.newsServiceLoading = true
+            this.defaultNewsServiceGet.getCustomNews(query).then((response) => {
+                // show only first 25 articles in response
+                this.customArticlesToDisplay = response.slice(0, 25)
+            }).catch((error) => {
+                console.log(error)
+            }).finally(() => {
+                this.newsServiceLoading = false
+            })
+        },
+        removeCustomNews() {
+            this.customNewsActive = false
+            this.customArticlesToDisplay = null
+        },
     },
     mounted() {
         this.defaultNewsServiceGet = new defaultNewsService(),
